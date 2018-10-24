dfa9371056433b77ddf54feb624a37a4dde66179
AMutaa <piusandru@gmail.com>
2018-10-24 16:17:39 -0400

    using css-module

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import Home from './Home';
 import './App.scss';
+import Button from './Button';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <button
+          className="btn"
+          onClick={() => alert('I am globally styled')}>
+          I am button 1 - Press Me
+          </button>
+        <Button />
         <Home />
       </div>
     );
